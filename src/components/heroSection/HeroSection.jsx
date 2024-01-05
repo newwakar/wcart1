1d5bea830d499cdb40ef3639046571581811f8ca
newwakar <127464384+newwakar@users.noreply.github.com>
2024-01-05 22:26:28 +0530

    Update HeroSection.jsx

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HeroSection() {
   return (
     <div>
-        <img src="https://raw.githubusercontent.com/newwakar/shopcart/main/public/w%20-%20cart.png" alt="" />
+        <img src="/main/public/w%20-%20cart.png" alt="" />
     </div>
   )
 }
