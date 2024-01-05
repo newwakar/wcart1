d902106fc6568a59ae793a43c2cb5b7d2ecbb25c
newwakar <127464384+newwakar@users.noreply.github.com>
2024-01-05 18:58:03 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -181,7 +181,7 @@ function Navbar() {
                   <a href="#" className="flex items-center text-gray-700 ">
                     <img
                       className="inline-block w-10 h-10 rounded-full"
-                      src="https://overreacted.io/static/profile-pic-c715447ce38098828758e525a1128b87.jpg"
+                      src="public/wakar.jpg"
                       alt="Dan_Abromov" />
                   </a>
                 </div>
