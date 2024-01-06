4ebe02888e5917a30167ebc1fd2ad943f316297f
newwakar <127464384+newwakar@users.noreply.github.com>
2024-01-06 16:49:29 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -64,7 +64,7 @@ function Navbar() {
                 <div className="space-y-6 border-t border-gray-200 px-4 py-6">
 
                   <section className="text-sm font-medium text-gray-900 " style={{ color: mode === 'dark' ? 'white' : '', }}>
-                    {user && user.user && user.user.email ? user.user.email : 'Guest}
+                    {user && user.user && user.user.email ? user.user.email : 'Guest'}
                   </section>
 
                   <Link to={'/allproducts'} className="text-sm font-medium text-gray-900 " style={{ color: mode === 'dark' ? 'white' : '', }}>
